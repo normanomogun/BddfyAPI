9b4d0dcfd649bbbf50c7ac33fc7a03817ab80e14
norge <norgee231@yahoo.com>
2019-07-25 06:04:50 +0100

    Add project files.

---
+++ APITests/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApiFramework;
+
+namespace APITests
+{
+    public class User : BaseClass
+    {
+        public data data { get; set; }
+        //public IEnumerable< data> dataList { get; set; }
+        //public string  name { get; set; }
+        //public string job { get; set; }
+    }
+}
---
+++ ApiFramework/ApiHelper.cs
@@ -0,0 +1,47 @@
+﻿using RestSharp;
+using System.Configuration;
+using Newtonsoft.Json;
+
+namespace ApiFramework
+{
+    public class ApiHelper
+    {
+        private RestClient _restClient;
+        private RestRequest _restRequest;
+        private IRestResponse _restResponse;
+        private string _apiUrl;
+        public string BaseUrl => ConfigurationManager.AppSettings["BaseUrl"];
+
+        public string SetApiEndpoint(string partUrl)
+        {
+            _apiUrl = BaseUrl + partUrl;
+            return _apiUrl;
+        }
+
+        public RestRequest MakeAGetRequest()
+        {
+            _restRequest = new RestRequest(Method.GET);
+            return _restRequest;
+        }
+
+        public RestRequest MakeAPostRequest(string jsonString)
+        {
+            _restRequest = new RestRequest(Method.POST);
+            _restRequest.AddHeader("Accept", "application/json");
+            _restRequest.AddParameter("application/json", jsonString, ParameterType.RequestBody);
+            return _restRequest;
+        }
+
+        public IRestResponse GetResponse()
+        {
+            _restClient = new RestClient(_apiUrl);
+            _restResponse = _restClient.Execute(_restRequest);
+            return _restResponse;
+        }
+
+        public T DeserializeJsonObject<T>(string response) where T : BaseClass, new()
+        {
+            return JsonConvert.DeserializeObject<T>(response);
+        }
+    }
+}
